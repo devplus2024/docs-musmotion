0251eed32ea4541fe405052b3ef9ad48e326ea28
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-09-14 14:58:32 +0700

    docs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,13 +1,8 @@
-import { DocsLayout } from "fumadocs-ui/layout";
-import type { ReactNode } from "react";
-import { baseOptions } from "../layout.config";
-import { source } from "@/app/source";
-import type { Metadata } from "next";
-import "fumadocs-ui/style.css";
-export const metadata: Metadata = {
-  title: "Documention | MusMotion",
-  description: "MusicHub App",
-};
+import { DocsLayout } from 'fumadocs-ui/layout';
+import type { ReactNode } from 'react';
+import { baseOptions } from '../layout.config';
+import { source } from '@/app/source';
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,18 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import "fumadocs-ui/style.css";
-import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "@/components/ui/theme-provider";
-import Image from "next/image";
-import { Navigation } from "@/components/Navigation";
-import { CommandMenu } from "@/components/CommandMenu";
-import { Toaster } from "@/components/ui/sonner";
-import Providers from "@/components/ProgressBarProvider";
-import Footer from "@/components/footer";
-const inter = Inter({ subsets: ["latin"] });
-export const metadata: Metadata = {
-  title: "Home | MusMotion",
-  description: "MusMotion - Home",
-};
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+import './global.css';
+import { RootProvider } from 'fumadocs-ui/provider';
+import { Inter } from 'next/font/google';
+import type { ReactNode } from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <html
-        suppressHydrationWarning
-        lang="en"
-        className={`${GeistSans.className} custom_command_scroll dark`}
-        style={{ colorScheme: "dark" }}
-      >
-        <head />
-        <body className="relative antialiased">
-          <Providers>
-            <ThemeProvider enableSystem attribute="class" defaultTheme="system">
-              <Navigation />
-              <Toaster
-                className=""
-                closeButton
-                hotkey={["alt + C"]}
-                toastOptions={{
-                  unstyled: false,
-                  classNames: {
-                    cancelButton: "bg-orange-400",
-                    closeButton:
-                      "dark:bg-black transition-[background] transition-colors  ease-out duration-500 bg-white hover:bg-muted dark:hover:bg-white dark:hover:text-black",
-                  },
-                }}
-              />
-              {children}
-              <Footer />
-            </ThemeProvider>
-          </Providers>
-        </body>
-      </html>
-    </>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body>
+        <RootProvider>{children}</RootProvider>
+      </body>
+    </html>
   );
 }
